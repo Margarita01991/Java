5c79d0a91e93afb5e905a1363000d3d89021b19e
Margarita <margarita27011991@gmail.com>
2022-11-26 13:42:08 +0300

    add homework 01

--- Homework01/Task03.java
+++ Homework01/Task03.java
@@ -3,31 +3,27 @@ import java.util.Scanner;
 // Реализовать простой калькулятор
 public class Task03 {
     public static void main(String[] args) {
-        System.out.println("Введите цифру команды:"+
-        "\n"+"1 сложение"+
-        "\n"+"2 вычетание"+
-        "\n"+"3 умножение"+
-        "\n"+"4 деление");
         Scanner in = new Scanner(System.in);
-        int command = in.nextInt();
         System.out.print("Введите первое число: ");
         int num1 = in.nextInt();
+        System.out.println("Введите операцию: + - * /");
+        String operand = in.next();
         System.out.print("Введите второе число: ");
         int num2 = in.nextInt();
         in.close();
-        if (command == 1){
+        if (operand.equals("+")){
             int result = num1 + num2;
             System.out.println(""+num1+"+"+num2+"="+result+"");
         }
-        if (command == 2){
+        if (operand.equals("-")){
             int result = num1 - num2;
             System.out.println(""+num1+"-"+num2+"="+result+"");
         }
-        if (command == 3){
+        if (operand.equals("*")){
             int result = num1 * num2;
             System.out.println(""+num1+"*"+num2+"="+result+"");
         }
-        if (command == 4){
+        if (operand.equals("/")){
             if (num2 !=0){
             int result = num1 / num2;
             System.out.println(""+num1+"/"+num2+"="+result+"");
