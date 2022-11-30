cef1097dd359927623135d6c0dd3ddb20a82412b
Margarita <margarita27011991@gmail.com>
2022-12-01 00:57:09 +0300

    add file

--- lessonJava/lection01.java
+++ lessonJava/lection01.java
@@ -498,3 +498,42 @@ public class Program {
  * 
  * 
  */
+// import java.util.Scanner;
+
+// // Реализовать простой калькулятор
+// public class Task03 {
+//     public static void main(String[] args) {
+//         System.out.println("Введите цифру команды:"+
+//         "\n"+"1 сложение"+
+//         "\n"+"2 вычетание"+
+//         "\n"+"3 умножение"+
+//         "\n"+"4 деление");
+//         Scanner in = new Scanner(System.in);
+//         int command = in.nextInt();
+//         System.out.print("Введите первое число: ");
+//         int num1 = in.nextInt();
+//         System.out.print("Введите второе число: ");
+//         int num2 = in.nextInt();
+//         in.close();
+//         if (command == 1){
+//             int result = num1 + num2;
+//             System.out.println(""+num1+"+"+num2+"="+result+"");
+//         }
+//         if (command == 2){
+//             int result = num1 - num2;
+//             System.out.println(""+num1+"-"+num2+"="+result+"");
+//         }
+//         if (command == 3){
+//             int result = num1 * num2;
+//             System.out.println(""+num1+"*"+num2+"="+result+"");
+//         }
+//         if (command == 4){
+//             if (num2 !=0){
+//             int result = num1 / num2;
+//             System.out.println(""+num1+"/"+num2+"="+result+"");
+//             } else {
+//                 System.out.println("На 0 делить нельзя");
+//             }
+//         }
+//     }
+// }
---
+++ lessonJava/lection03.java
@@ -0,0 +1,39 @@
+ /* import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.SimpleFormatter;
+
+public class lection03 {
+    public static void main(String[] args) {
+        Логирование
+        Логи содержат системную информацию работы программного или аппаратного комплекса.
+        В них  записываются действия разного приоритета от  пользователя, или программного продукта.
+        Процесс ведения логов называют “логированием” (журналированием).
+
+        Логирование. Использование. Основы
+        Использование
+        Logger logger = Logger.getLogger()
+
+        Уровни важности
+        INFO, DEBUG, ERROR, WARNING и др.
+        
+        Вывод
+        ConsoleHandler info = new ConsoleHandler();
+        log.addHandler(info);
+
+        Формат вывода: структурированный, или произвольный
+        XMLFormatter, SimpleFormatter
+
+        например: 
+        Logger logger = Logger.getLogger(Ex0043.class.getName());
+        logger.setLevel(Level.INFO);
+        ConsoleHandler ch = new ConsoleHandler();
+        logger.addHandler(ch);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        ch.setFormatter(sFormat);
+        logger.log(Level.WARNING, "Тестовое логирование");
+        logger.info("Тестовое логирование");
+
+    }
+}
+*/
